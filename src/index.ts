81e9bd736faeae31eabd00b1dda7f98ee1cc9cdc
Alan Fitzpatrick <alajfit@gmail.com>
2019-12-17 20:20:29 +0000

    Updated base of the Class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("Beginning of something beautiful");
+class FIF {
+    private inflight = new Map()
+
+    constructor() {}
+
+    private hashCode(code: string):string {
+        return Array.from(code).reduce((s, c) => Math.imul(31, s) + c.charCodeAt(0) | 0, 0).toString()
+    }
+
+    private uniqueKey(url: string, options: RequestInit, unique: boolean) {
+        return this.hashCode(url + (options.method || 'GET') + JSON.stringify(options) + (unique ? Math.random() : ''))
+    }
+
+    private newFlight() {
+
+    }
+
+    private inFlight() {
+
+    }
+
+    private cleanUp() {
+
+    }
+    
+    public fetch(url: string, options: RequestInit = {}, unqiue = false) {
+        return new Promise((resolve, reject) => {
+            fetch(url, { ...options })
+                .then(data => resolve(data))
+                .catch(err => reject(err))
+        })
+    }
+}
