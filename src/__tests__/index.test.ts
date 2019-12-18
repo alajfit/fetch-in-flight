44e435ae4288d952c516530472a05f5e61e51ef4
Alan Fitzpatrick <alajfit@gmail.com>
2019-12-18 19:19:58 +0000

    Fixed test setup

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,7 @@
+test('Prevent Multiple Network Requests', () => {
+    expect(true).toBe(true)
+})
+
+test('Resolve all callers with the returned data', () => {
+    expect(true).toBe(true)
+})
--- tests/index.ts
+++
@@ -1,5 +0,0 @@
-describe('Fetch In Flight', () => {
-    it('Should Work', () => {
-        expect(true).toBe(true)
-    })
-})
