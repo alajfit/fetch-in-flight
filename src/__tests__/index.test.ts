df1039de7f80a74202106ab974d2824f50f9f9bf
Alan Fitzpatrick <alan.fitzpatrick@jet.com>
2020-01-23 18:35:02 +0000

    added jest mocking dep

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,23 +1,25 @@
-import FIF from '../index';
+import FIF from "../index";
+import fetch from "node-fetch";
 
-const fetcher = new FIF()
+const fetcher = new FIF(fetch);
 
-describe('Fetch In Flight', () => {
-    let fetch
+describe("Fetch In Flight", () => {
+  let fetchIt;
 
-    beforeEach(() => {
-        fetch = fetcher.fetch
-    })
+  beforeEach(() => {
+    fetchIt = fetcher.fetch;
+  });
 
-    test('Prevent Multiple Network Requests', () => {
-        expect(true).toBe(true)
-    })
+  test("Prevent Multiple Network Requests", () => {
+    fetchIt("");
+    expect(true).toBe(true);
+  });
 
-    test('Resolve all callers with the returned data', () => {
-        expect(true).toBe(true)
-    })
+  test("Resolve all callers with the returned data", () => {
+    expect(true).toBe(true);
+  });
 
-    test('Cache for given duration if time set', () => {
-        expect(true).toBe(true)
-    })
-})
+  test("Cache for given duration if time set", () => {
+    expect(true).toBe(true);
+  });
+});
