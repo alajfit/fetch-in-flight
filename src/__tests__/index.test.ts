c1e8d6d38ef63e198c42e28bea0c2c0ebb25647c
Alan Fitzpatrick <alan.fitzpatrick@jet.com>
2020-01-23 18:19:35 +0000

    Adding Cache test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -12,8 +12,12 @@ describe('Fetch In Flight', () => {
     test('Prevent Multiple Network Requests', () => {
         expect(true).toBe(true)
     })
-    
+
     test('Resolve all callers with the returned data', () => {
+        expect(true).toBe(true)
+    })
+
+    test('Cache for given duration if time set', () => {
         expect(true).toBe(true)
     })
 })
