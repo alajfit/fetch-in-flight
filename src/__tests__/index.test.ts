aae0d571bb038064e4ee8cb1cafe0d82c7ef62cd
Alan Fitzpatrick <alan.fitzpatrick@jet.com>
2020-01-23 22:33:19 +0000

    Added real test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -5,8 +5,6 @@ const fetchMock = fetch as FetchMock;
 const fetcher = new FIF(fetch);
 
 describe("Fetch In Flight", () => {
-  const fetchIt = fetcher.fetch;
-
   beforeEach(() => {
     fetchMock.resetMocks();
   });
@@ -17,18 +15,10 @@ describe("Fetch In Flight", () => {
         new Promise(resolve => setTimeout(() => resolve({ body: "ok" }), 1000))
     );
 
-    fetchIt("http://www.google.com");
-    fetchIt("http://www.google.com");
-    fetchIt("http://www.google.com");
+    fetcher.fetch("http://www.google.com");
+    fetcher.fetch("http://www.google.com");
+    fetcher.fetch("http://www.google.com");
 
     expect(fetchMock.mock.calls.length).toEqual(1);
-  });
-
-  test("Resolve all callers with the returned data", () => {
-    expect(true).toBe(true);
-  });
-
-  test("Cache for given duration if time set", () => {
-    expect(true).toBe(true);
   });
 });
